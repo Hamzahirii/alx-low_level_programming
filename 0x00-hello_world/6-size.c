4264b8bbb6180c4a82ac836094659e1b49207872
gyro <ezzahiriha@gmail.com>
2023-07-13 10:01:44 -0700

    gcc 6-size.c -m64 -o size64 2> /tmp/6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always0 (Success
+*/
+
+int mai(void)
+{       
+        printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("Size of a char: %lu bytes(s)\n", sizeof(float));
+        return(0);
+} 
