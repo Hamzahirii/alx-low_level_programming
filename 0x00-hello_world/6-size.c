2c821ccdd84421030675a390c6882d77d323af82
gyro <ezzahiriha@gmail.com>
2023-07-13 18:02:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,5 +13,5 @@ int mai(void)
 	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
 	printf("Size of a char: %lu bytes(s)\n", sizeof(float));
-        return(0);
+        return (0);
 } 
